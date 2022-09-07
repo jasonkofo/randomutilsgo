11ee7b4df0565f0a5c30448aad9db649133a8438
Jason K. George <jasonkofo@gmail.com>
2022-09-07 15:36:22 +0200

    Minor alterations of the strings that were generated

--- string_test.go
+++ string_test.go
@@ -6,13 +6,13 @@ import (
 	"time"
 )
 
-func TestGeneratePassword(t *testing.T) {
+func TestGenerateString(t *testing.T) {
 	var (
 		PWD_LEN = 100
 		start   = time.Now()
 		pwd     = GenerateRandomString(PWD_LEN)
 	)
-	fmt.Printf("Generating password with %d digits took %f seconds\n", PWD_LEN, time.Since(start).Seconds())
+	fmt.Printf("Generating random string with %d digits took %f seconds\n", PWD_LEN, time.Since(start).Seconds())
 	if len(pwd) != PWD_LEN {
 		t.Errorf("Did not generate the correct number of digits")
 	}
